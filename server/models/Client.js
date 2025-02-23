cb469c119a40ec4742997e9b0b306d36556bd477
mziliNassim <mzilinassim@gmail.com>
2025-02-23 03:47:27 +0000

    update client model: 'Adresse : require to false'!!

--- server/models/Client.js
+++ server/models/Client.js
@@ -5,7 +5,7 @@ const clientSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  adresse: { type: String, required: true },
+  adresse: { type: String, required: false },
   active: { type: Boolean, default: true },
   role: { type: String, default: "client", enum: ["client", "admin"] },
 });
