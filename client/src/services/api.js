da401a14fea560f3778ed6b55e8893b664e1cfa4
mziliNassim <mzilinassim@gmail.com>
2025-04-25 17:26:17 +0100

    Auth reset password!

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 // "http://localhost:8888";
 
-export const clientsApiURL = "http://localhost:8888/api/clients";
+export const clientsApiURL =
+  "https://gestion-bibliotique-pfe.netlify.app/api/clients";
 
 export const livresApiURL =
   "https://gestion-bibliotique-pfe.netlify.app/api/livres";
