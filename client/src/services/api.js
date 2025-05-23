0ad546725d37b5ea439953d45f84045377d31d32
mziliNassim <mzilinassim@gmail.com>
2025-05-23 23:40:33 +0100

    envirement!

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 const serverURL =
-  import.meta.env.VITE_ENV === "dev"
-    ? import.meta.env.VITE_SERVER_URL_DEV
+  !import.meta.env.VITE_ENV || import.meta.env.VITE_ENV === "dev"
+    ? "http://localhost:8888"
     : import.meta.env.VITE_SERVER_URL_PROD;
 
 export const clientsApiURL = `${serverURL}/api/clients`;
